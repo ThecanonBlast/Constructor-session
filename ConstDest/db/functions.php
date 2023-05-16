7b4f423962976f911ab3dbc3708bd58286c2f769
TheCannonBlast <128511167+ThecanonBlast@users.noreply.github.com>
2023-05-16 09:34:33 -0500

    Update functions.php

--- ConstDest/db/functions.php
+++ ConstDest/db/functions.php
@@ -1,22 +1,32 @@
 <?php
 
-if(isset($_POST['submit'])){
-    if(isset($_POST['usr'])){
-        include_once('ClassPoo.php');
-    
-        $user =$_POST['usr'];
-        $pass =$_POST['pss'];
-        $login = new Login();
-        $login ->entrarDatos($user, $pass);
-        $login ->logear();
-        $response = $login->logear();
-    
-        if($response === "true"){
-            header("location:../Home.php");
-            echo'<script language"javascript">alert("Bienvenido Usuario")</script>';
-        } else{
-            header("location:../login.php");
-            echo'<script language"javascript">alert("Error en datos")</script>';
+class Login {
+    private $username;
+    private $password;
+
+    public function __construct($username, $password) {
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function validarCredenciales($username, $password) {
+        if ($this->username === $username && $this->password === $password) {
+            return true;
+        } else {
+            return false;
         }
     }
+
 }
+
+
+// class Logout{
+//     function logout() {
+        
+//         session_destroy();
+//         unset($submit_login['email']);
+//         unset($submit_login['password']);
+//     }
+    
+
+// }
