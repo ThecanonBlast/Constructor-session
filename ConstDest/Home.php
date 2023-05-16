945d1cddaaea439e13b9e2cc47c35a21f5b51f69
TheCannonBlast <128511167+ThecanonBlast@users.noreply.github.com>
2023-05-16 09:33:08 -0500

    Update Home.php

--- ConstDest/Home.php
+++ ConstDest/Home.php
@@ -13,7 +13,9 @@
 <body>
     <div class="container-fluid">
     <div class="d-flex justify-content-center align-items-center vh-100">
-      <button class="btn btn-danger">Eliminar datos</button>
+      <form action="./db/ClassPoo.php" method="post">
+      <button  type="submit" name="submit_logout" class="btn btn-danger">Eliminar datos</button>
+      </form>
     </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
